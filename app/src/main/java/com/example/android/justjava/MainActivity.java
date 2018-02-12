b4b137f55fb43e2abd2772d74f68074defab73e1
petrofang <gilescooper@gmail.com>
2018-02-12 10:08:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.justjava;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+
+
+
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {;
+        display(quantity);
+        int price;
+        price = quantity/7 + 100;
+        displayPrice(price);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    // when plus is clicked
+    public void increment(View view){
+        quantity++;
+        display(quantity);
+    }
+
+    // when minus is clicked
+    public void decrement(View view) {
+        if (quantity != 0) {
+            quantity--;
+            display(quantity);
+        } else {
+            // WTF?
+        }
+    }
+
+
+}
